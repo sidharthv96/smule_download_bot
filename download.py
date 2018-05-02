6208a19ec405fde83c34f2e70b724d4de44a0130
Sidharth V <sidharthv96@gmail.com>
2018-05-02 20:43:37 +0530

    Initial

--- download.py
+++ download.py
@@ -1,6 +1,5 @@
 import urllib.request
 import requests
-x = requests.get('http://www.smule.com/p/1695845141_2189897499').text
+x = requests.get('URL').text
 print(x[x.find('<title>') + 7 : x.find('</title>')-9]+".m4a")
-# print(x.split('twitter:player:stream" content="')[1].split('">')[0].replace('amp;', ''))
 urllib.request.urlretrieve(x.split('twitter:player:stream" content="')[1].split('">')[0].replace('amp;', ''), x[x.find('<title>') + 7 : x.find('</title>')-9]+".m4a")
